5298a26c7a57c81da1db54908c2b0e53b5ec48cb
Roman Rudolf <roman_rudolf@icloud.com>
2025-02-03 13:00:48 +0000

    added errorh-handler to index.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { json } from 'body-parser';
+
 import { currentUserRouter } from './routes/currentUser';
 import { signInRouter } from './routes/signin';
 import { signOutRouter } from './routes/signout';
 import { signUpRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 app.use(json());
@@ -12,6 +14,8 @@ app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');

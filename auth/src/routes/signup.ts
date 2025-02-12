87baf4977f14c42ccb830531065b903c3808d682
Roman Rudolf <roman_rudolf@icloud.com>
2025-02-12 11:51:11 +0000

    adding jsonwebtoken

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
@@ -33,8 +35,18 @@ router.post(
     }
 
     const user = User.build({ email, password });
-
     await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign({
+      id: user.id,
+      email: user.email
+    }, process.env.JWT_KEY!);
+
+    // Store JWT on session object
+    req.session = {
+      jwt: userJwt
+    };
 
     res.status(201).send(user);
   }

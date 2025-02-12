5b5229aaa141c97f25a64d089de4c8d5896dc74b
Roman Rudolf <roman_rudolf@icloud.com>
2025-02-12 20:46:44 +0000

    started signin route

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -9,8 +9,7 @@ import { BadRequestError } from '../errors/bad-request-error';
 const router = express.Router();
 
 router.post(
-  '/api/users/signup', 
-  [
+  '/api/users/signup', [
     body('email')
       .isEmail()
       .withMessage('Email must be valid'),
@@ -18,8 +17,8 @@ router.post(
       .trim()
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20 characters')
-  ], 
-  async (req: Request, res: Response) => {
+  ], async (req: Request, res: Response) => {
+    
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {

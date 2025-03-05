ae5f4bcdaf1481c77a9687fbf8392e47787bee4e
Roman Rudolf <roman_rudolf@icloud.com>
2025-03-05 21:59:25 +0000

    added bootstrap and started signup route

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,16 @@
+const Signup = () => {
+  return <form>
+    <h1>Sign Up</h1>
+    <div>
+      <label>Email</label>
+      <input />
+    </div>
+    <div>
+      <label>Password</label>
+      <input />
+    </div>
+    <button>Sign Up</button>
+  </form>
+}
+
+export default Signup;

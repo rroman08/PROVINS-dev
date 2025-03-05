c4d0c51394c459d58dcb8fb776de1d2087aabf64
Roman Rudolf <roman_rudolf@icloud.com>
2025-03-05 22:11:00 +0000

    added sign up functionality

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,16 +1,46 @@
+import { useState } from 'react';
+
 const Signup = () => {
-  return <form>
-    <h1>Sign Up</h1>
-    <div>
-      <label>Email</label>
-      <input />
-    </div>
-    <div>
-      <label>Password</label>
-      <input />
-    </div>
-    <button>Sign Up</button>
-  </form>
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch('/api/users/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, password })
+    });
+
+    const data = await response.json();
+    console.log(data);
+  };
+
+  return 
+    <form onSubmit={onSubmit}>
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input 
+          value={email} 
+          onChange={e => setEmail(e.target.value)}
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input 
+          value={password} 
+          onChange={e => setPassword(e.target.value)}
+          type="password" 
+          className="form-control"
+        />
+      </div>
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
 }
 
 export default Signup;

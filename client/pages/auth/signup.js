a5885d26ceb46fd25c7740668dc37fce3125030b
Roman Rudolf <roman_rudolf@icloud.com>
2025-03-06 11:06:55 +0000

    sign up funtionality linked to endpoint

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,26 +1,26 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState('');
 
-  const onSubmit = async (event) => {
+  const onSubmit = async event => {
     event.preventDefault();
 
-    const response = await fetch('/api/users/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ email, password })
-    });
-
-    const data = await response.json();
-    console.log(data);
+    try {
+      const response = await axios.post('/api/users/signup', {
+        email, password
+      });
+      console.log(response.data);
+    }
+    catch (error) { 
+      setErrors(error.response.data.errors);
+    }
   };
 
-  return 
-    <form onSubmit={onSubmit}>
+  return <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className="form-group">
         <label>Email Address</label>
@@ -39,6 +39,16 @@ const Signup = () => {
           className="form-control"
         />
       </div>
+      {errors.length > 0 && (
+        <div className='alert alert-danger'>
+          <h4>Ooops....</h4>
+          <ul className='my-0'>
+            {errors.map(error => (
+              <li key={error.message}>{error.message}</li>
+              ))}
+          </ul>
+        </div>
+      )}
       <button className="btn btn-primary">Sign Up</button>
     </form>
 }

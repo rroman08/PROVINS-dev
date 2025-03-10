f847c2c0d713011f07bd1ed8b2df9ba46ac2353b
Roman Rudolf <roman_rudolf@icloud.com>
2025-03-10 11:49:07 +0000

    sorted out if request comes from browser or from client server

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,30 @@
-const Landing = () => {
-  return <h1>Landing Page</h1>
-}
+import axios from "axios";
 
-export default Landing;
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  return <h1>Landing Page</h1>;
+};
+
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    // On the server -> Requests should be made to http://SERVICENAME.NAMESPACE.svc.cluster.local
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      { 
+        headers: req.headers,
+      }
+    ).catch((err) => {
+      console.log(err.message);
+    });
+    return data;
+  } else {
+    // On the browser -> requests can be made with a base url of '' (browser prepends correct base url)
+    const { data } = await axios.get('/api/users/currentuser').catch((err) => {
+      console.log(err.message);
+    });
+    // response is null in the data field, if the user is not logged in
+    return data;
+  }
+}; 
+
+export default LandingPage;

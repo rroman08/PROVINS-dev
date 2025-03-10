dd42fae4bad0e344b1b69677da8fa49e47d6c8f7
Roman Rudolf <roman_rudolf@icloud.com>
2025-03-10 12:35:26 +0000

    refactore getInitialProps from component to top hierarchy appComponent

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import buildClient from '../api/build-client';  
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps }) => {
+  return <div>
+    <h1>Header!</h1>
+    <Component {...pageProps} />
+    </div>;
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  // build an axios client with the context
+  const client = buildClient(appContext.ctx);
+  // make a request to the current user route
+  const { data } = await client.get("/api/users/currentuser");
+  
+  // get the initial props of the component
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+  return {
+    pageProps,
+    ...data
+  };
+};
+
+export default AppComponent;

2106154ef5433dcba655c72aed46fee05f04b76b
Roman Rudolf <roman_rudolf@icloud.com>
2025-03-11 13:05:25 +0000

    finished test suite for new product (creation) route

--- products/src/routes/__test__/new.test.ts
+++ products/src/routes/__test__/new.test.ts
@@ -1,5 +1,7 @@
 import request from 'supertest';
+
 import { app } from '../../app';
+import { Product } from '../../models/product';
 
 it('has a route handler listening to /api/products for post requests', async () => {
   const response = await request(app)
@@ -64,9 +66,23 @@ it('returns an error if an invalid price is provided', async () => {
 });
 
 it('creates a product with valid inputs', async () => {
-  const response = await request(app)
-    .post('/api/products')
-    .send({});
+  // check how many records are in the database
+  let products = await Product.find({});
+  expect(products.length).toEqual(0);
 
-  expect(response.status).not.toEqual(404);
+  const title = 'Shoes';
+
+  await request(app)
+    .post('/api/products')
+    .set('Cookie', global.signup())
+    .send({
+      title: title,
+      price: 99.99
+    })
+    .expect(201);
+
+  products = await Product.find({});
+  expect(products.length).toEqual(1);
+  expect(products[0].price).toEqual(99.99);
+  expect(products[0].title).toEqual(title);
 });
--- products/src/routes/new.ts
+++ products/src/routes/new.ts
@@ -2,13 +2,29 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@provins/common';
 
+import { Product } from '../models/product';
+
 const router = express.Router();
 
-router.post('/api/products', requireAuth, [
-  body('title').not().isEmpty().withMessage('Title is required'),
-  body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than 0')
-], validateRequest, async (req: Request, res: Response) => {
-  res.sendStatus(200);
+router.post(
+  '/api/products', 
+  requireAuth, [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than 0')
+  ], 
+  validateRequest, 
+  async (req: Request, res: Response) => {
+  
+  const { title, price } = req.body;
+
+  const product = Product.build({
+    title,
+    price,
+    userId: req.currentUser!.id
+  });
+  await product.save();
+
+  res.status(201).send(product);
 });
 
 export { router as createProductRouter };

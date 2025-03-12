632c7b67bd821487dbc797b2a8585af4118b885b
Roman Rudolf <roman_rudolf@icloud.com>
2025-03-12 13:25:27 +0000

    created a listener

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,22 @@
+import nats from 'node-nats-streaming';
+
+console.clear();
+
+const client = nats.connect('provins', '123', {
+  url: 'http://localhost:4222',
+});
+
+client.on('connect', () => {
+  console.log('Listener connected to NATS');
+
+  const subscription = client.subscribe('product:created');
+
+  // message = event
+  subscription.on('message', (msg) => {
+    const data = msg.getData();
+
+    if (typeof data === 'string') {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
+  });
+});
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,5 +1,7 @@
 import nats from 'node-nats-streaming';
 
+console.clear();
+
 const client = nats.connect('provins', 'abc', {
   url: 'http://localhost:4222',
 });

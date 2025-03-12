ae6433126f40a2264782b71700069edd211e4240
Roman Rudolf <roman_rudolf@icloud.com>
2025-03-12 13:08:37 +0000

    published first event

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -6,4 +6,20 @@ const client = nats.connect('provins', 'abc', {
 
 client.on('connect', () => {
   console.log('Publisher connected to NATS');
+
+  // NATS expects the data to be a string
+  // Other word for data/event is also message (used in the NATS documentation)
+  const data = JSON.stringify({
+    id: '123',
+    title: 'Running Shoes',
+    price: 99.99,
+  });
+
+  client.publish('product:created', data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('Event published');
+    }
+  });
 });

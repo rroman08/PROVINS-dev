44204560a9ff292d0f4b388cfd5bfef051e678dd
Roman Rudolf <roman_rudolf@icloud.com>
2025-03-12 14:01:11 +0000

    manually ack previous events

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -24,5 +24,7 @@ client.on('connect', () => {
     if (typeof data === 'string') {
       console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
     }
+
+    msg.ack();
   });
 });

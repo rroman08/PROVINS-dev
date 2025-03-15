351996678d093097d86f92583188f4ce54b00cd2
Roman Rudolf <roman_rudolf@icloud.com>
2025-03-15 22:36:03 +0000

    creted interfaces to enfore listener subjects

--- nats-test/src/events/product-created-listener.ts
+++ nats-test/src/events/product-created-listener.ts
@@ -1,9 +1,11 @@
 import { Message } from 'node-nats-streaming';
 
 import { Listener } from './base-listener';
+import { ProductCreatedEvent } from './product-created-event';
+import { Subjects } from './subjects';
 
-export class ProductCreatedListener extends Listener {
-  subject = 'product:created';
+export class ProductCreatedListener extends Listener<ProductCreatedEvent> {
+  subject: Subjects.ProductCreated = Subjects.ProductCreated;
   queueGroupName = 'queue-group';
 
   onMessage(data: any, msg: Message) {

4ebf3c12b30bdbbd9f0f78bf247b78e9b2800cbe
Roman Rudolf <roman_rudolf@icloud.com>
2025-03-15 22:48:38 +0000

    created abstract publisher class

---
+++ nats-test/src/events/base-publisher.ts
@@ -0,0 +1,27 @@
+import { Stan } from 'node-nats-streaming';
+
+import { Subjects } from './subjects';
+
+interface Event {
+  subject: Subjects;
+  data: any;
+}
+
+export abstract class Publisher<T extends Event> {
+  abstract subject: T['subject'];
+  private client: Stan;
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  publish(data: T['data']) {
+    this.client.publish(this.subject, data, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('Event published');
+      }
+    });
+  }
+}

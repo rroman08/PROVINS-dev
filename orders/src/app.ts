eadf66f4c4691d6bbc485e10438daf8a69b8564d
Roman Rudolf <roman_rudolf@icloud.com>
2025-03-17 14:26:46 +0000

    installed dependencies and created k8s deployment and service

---
+++ orders/src/app.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+import { 
+  errorHandler, 
+  NotFoundError, 
+  currentUser 
+} from '@provins/common';
+
+import { createProductRouter } from './routes/new';
+import { showProductRouter } from './routes/show';
+import { indexProductRouter } from './routes/index';
+import { updateProductRouter } from './routes/update';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(
+  cookieSession({ 
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+  })
+);
+app.use(currentUser);
+
+app.use(createProductRouter);
+app.use(showProductRouter);
+app.use(indexProductRouter);
+app.use(updateProductRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };

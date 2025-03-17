c3c7076a8f29602f31eeda41b723818ad8690af0
Roman Rudolf <roman_rudolf@icloud.com>
2025-03-17 22:58:26 +0000

    wrote cancel order tests

---
+++ orders/src/routes/__test__/cancel.test.ts
@@ -0,0 +1,30 @@
+import request from 'supertest';
+
+import { app } from '../../app';
+import { Product } from '../../models/product';
+import { Order, OrderStatus } from '../../models/order';
+
+it ('changes status of order to cancelled', async () => {
+  const product = Product.build({
+    title: 'Running Shoes',
+    price: 99.99
+  });
+  await product.save();
+
+  const user = global.signup();
+  const { body: orderResponse } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ productId: product.id })
+    .expect(201);
+
+  await request(app)
+    .patch(`/api/orders/${orderResponse.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(200);
+
+  const updatedOrder = await Order.findById(orderResponse.id);
+
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});

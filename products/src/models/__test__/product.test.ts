ca650b3796f4a5ef2e0ef7de1f58e7fb37a5e08c
Roman Rudolf <roman_rudolf@icloud.com>
2025-03-18 21:44:52 +0000

    tested version of product

---
+++ products/src/models/__test__/product.test.ts
@@ -0,0 +1,40 @@
+import { Product } from '../product';
+
+it ('implements optimistic concurrency control (OCC)', async () => {
+  const product = Product.build({
+    title: 'Running Shoes',
+    price: 99.99,
+    userId: 'testtesttest',
+  });
+  await product.save();
+  // Fetch product twice
+  const firstInstance = await Product.findById(product.id);
+  const secondInstance = await Product.findById(product.id);  
+  // Make two separate changes to the fetched products
+  firstInstance!.set({ price: 109.99 });
+  secondInstance!.set({ price: 77.99 });
+  // Save first fetched product
+  await firstInstance!.save();
+  // Save second fetched product (must have outdated version)
+  try {
+    await secondInstance!.save();
+  } catch (err) {
+    return;
+  }
+
+  throw new Error('*** This should not be reached ***');
+});
+
+it ('increments the version correctly', async () => {
+  const product = Product.build({
+    title: 'Running Shoes',
+    price: 99.99,
+    userId: 'testtesttest',
+  });
+  await product.save();
+  expect(product.version).toEqual(0);
+  await product.save();
+  expect(product.version).toEqual(1);
+  await product.save();
+  expect(product.version).toEqual(2);
+});

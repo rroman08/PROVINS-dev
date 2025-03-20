e31bb8d6f981d9eefb06795eaa96aa86c7f86a1d
Roman Rudolf <roman_rudolf@icloud.com>
2025-03-20 13:26:50 +0000

    created expiration complete listener implementation

---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,40 @@
+import { Listener, Subjects, ExpirationCompleteEvent } from '@provins/common';
+import { Message } from 'node-nats-streaming';
+
+import { queueGroupName } from './queue-group-name';
+import { Order, OrderStatus } from '../../models/order';
+import { OrderCancelledPublisher } from '../publishers/order-cancelled-publisher';
+
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+    const order = await Order.findById(data.orderId).populate('product');
+
+    if (!order) {
+      throw new Error('Order not found');
+    }
+
+    if (order.status === OrderStatus.Complete) {
+      return msg.ack();
+    }
+
+    order.set({
+      status: OrderStatus.Cancelled,
+    });
+    await order.save();
+
+    await new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+      product: {
+        id: order.product.id,
+        price: order.product.price,
+      },
+    });
+
+    msg.ack();
+  }
+}

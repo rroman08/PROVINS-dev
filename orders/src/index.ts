2d09c92b8c92de01eba04cb8fa2099d7bc20261a
Roman Rudolf <roman_rudolf@icloud.com>
2025-03-20 13:38:52 +0000

    added expiration listener to index

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -4,6 +4,7 @@ import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
 import { ProductCreatedListener } from './events/listeners/product-created-listener';
 import { ProductUpdatedListener } from './events/listeners/product-updated-listener';
+import { ExpirationCompleteListener } from './events/listeners/expiration-complete-listener';
 
 const start = async () => {
 
@@ -44,6 +45,7 @@ const start = async () => {
 
     new ProductCreatedListener(natsWrapper.client).listen();
     new ProductUpdatedListener(natsWrapper.client).listen();
+    new ExpirationCompleteListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');

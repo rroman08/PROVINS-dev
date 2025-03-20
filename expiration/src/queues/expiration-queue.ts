078d881173660782bfb49ba62165427de9b3efd3
Roman Rudolf <roman_rudolf@icloud.com>
2025-03-20 13:12:35 +0000

    created expiration complete publisher and publish event in queue

--- expiration/src/queues/expiration-queue.ts
+++ expiration/src/queues/expiration-queue.ts
@@ -1,5 +1,8 @@
 import Queue from 'bull';
 
+import { ExpirationCompletePublisher } from '../events/publishers/expiration-complete-publisher';
+import { natsWrapper } from '../nats-wrapper';
+
 interface Payload {
   orderId: string;
 }
@@ -12,10 +15,9 @@ const expirationQueue = new Queue<Payload>(
 });
 
 expirationQueue.process(async (job) => {
-  console.log(
-    'I want to publish an expiration:complete event for orderId', 
-    job.data.orderId
-  );
+  new ExpirationCompletePublisher(natsWrapper.client).publish({
+    orderId: job.data.orderId
+  });
 });
 
 export { expirationQueue };

410d060be56f62720f851353833e8238471b5751
Roman Rudolf <roman_rudolf@icloud.com>
2025-03-25 22:11:54 +0000

    created test for order cancelled listnener..

--- payments/src/events/listeners/__test__/order-cancelled-listener.test.ts
+++ payments/src/events/listeners/__test__/order-cancelled-listener.test.ts
@@ -1,20 +1,26 @@
 import mongoose from 'mongoose';
 import { Message } from 'node-nats-streaming';
-import { OrderCreatedEvent, OrderStatus } from '@provins/common';
+import { OrderCancelledEvent, OrderStatus } from '@provins/common';
 
 import { natsWrapper } from '../../../nats-wrapper';
-import { OrderCreatedListener } from '../order-created-listener';
+import { OrderCancelledListener } from '../order-cancelled-listener';
 import { Order } from '../../../models/order';
 
 const setup = async () => {
-  const listener = new OrderCreatedListener(natsWrapper.client);
+  const listener = new OrderCancelledListener(natsWrapper.client);
 
-  const data: OrderCreatedEvent['data'] = {
+  const order = Order.build({
     id: new mongoose.Types.ObjectId().toHexString(),
-    version: 0,
     status: OrderStatus.Created,
-    expiresAt: Date(),
+    price: 99.99,
     userId: new mongoose.Types.ObjectId().toHexString(),
+    version: 0
+  });
+  await order.save();
+
+  const data: OrderCancelledEvent['data'] = {
+    id: order.id,
+    version: 1,
     product: {
       id: new mongoose.Types.ObjectId().toHexString(),
       price: 99.99
@@ -26,18 +32,17 @@ const setup = async () => {
     ack: jest.fn()
   };
 
-  return { listener, data, msg };
+  return { listener, data, msg, order };
 };
 
-it('replicates order info', async () => {
-  const { listener, data, msg } = await setup();
+it('updaes status of order', async () => {
+  const { listener, data, msg, order } = await setup();
 
   await listener.onMessage(data, msg);
 
-  const order = await Order.findById(data.id);
+  const updatedOrder = await Order.findById(order.id);
 
-  expect(order).toBeDefined();
-  expect(order!.price).toEqual(data.product.price);
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
 });
 
 it('acks message', async () => {

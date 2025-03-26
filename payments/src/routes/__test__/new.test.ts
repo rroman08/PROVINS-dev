0440e2067c4a69b5e6a846310520274605a9f9e4
Roman Rudolf <roman_rudolf@icloud.com>
2025-03-26 11:53:12 +0000

    created a test for new route

---
+++ payments/src/routes/__test__/new.test.ts
@@ -0,0 +1,55 @@
+import request from 'supertest';
+import mongoose from 'mongoose';
+import { OrderStatus } from '@provins/common';
+
+import { app } from '../../app';
+import { Order } from '../../models/order';
+
+it('returns 404 when trying to purchase a non-existing order', async () => {
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signup())
+    .send({
+      token: 'testtoken',
+      orderId: new mongoose.Types.ObjectId().toHexString()
+    }).expect(404);
+});
+
+it('returns 401 when trying to purchase someone elses order', async () => {
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    userId: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    price: 99.99,
+    status: OrderStatus.Created
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signup())
+    .send({
+      token: 'testtoken',
+      orderId: order.id
+    }).expect(401);
+});
+
+it('returns 400 when trying to purchase cancelled order', async () => {
+  const userId = new mongoose.Types.ObjectId().toHexString();
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    userId,
+    version: 0,
+    price: 99.99,
+    status: OrderStatus.Cancelled
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signup(userId))
+    .send({
+      orderId: order.id,
+      token: 'testtoken'
+    }).expect(400);
+});

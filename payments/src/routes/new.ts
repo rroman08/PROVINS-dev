e8546ceea810a86f46f7141506316e3b44869e2f
Roman Rudolf <roman_rudolf@icloud.com>
2025-03-26 22:21:05 +0000

    created payment-created publisher

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -12,6 +12,8 @@ import {
 import { Order } from '../models/order';
 import stripe from '../stripe';
 import { Payment } from '../models/payment';
+import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -58,7 +60,14 @@ router.post('/api/payments', requireAuth,
     });
     await payment.save();
 
-    res.status(204).send({ success: true });
+    // Publish payment created event
+    new PaymentCreatedPublisher(natsWrapper.client).publish({
+      id: payment.id,
+      orderId: payment.orderId,
+      stripeId: payment.stripeId
+    });
+
+    res.status(204).send({ id: payment.id });
   }
 );  
 

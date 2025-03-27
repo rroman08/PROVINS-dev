ad55f6912f6492f3ebaa1662768ea8a52a5982f4
Roman Rudolf <roman_rudolf@icloud.com>
2025-03-27 13:53:59 +0000

    list out product details and added purchase button

--- client/pages/products/[productId].js
+++ client/pages/products/[productId].js
@@ -2,7 +2,22 @@
 // The productId will be available in the context object
 
 const ProductShow = ({ product }) => {
-  return <div>Product Details</div>
+  return (
+    <div>
+      <h1>Product Details</h1>
+      <h3>Title: {product.title}</h3>
+      <h3>Price: £{product.price}</h3>
+      <button className="btn btn-primary">I want to purchase</button>
+    </div>
+  );
 }
+
+ProductShow.getInitialProps = async (context, client) => {
+  // context.query is an object that contains the wildcard values
+  const { productId } = context.query;
+  const { data } = await client.get(`/api/products/${productId}`);
+
+  return { product: data };
+};
 
 export default ProductShow;

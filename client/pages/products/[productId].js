67b49711af7d99a5bb3eccfe73704fd030b20f38
Roman Rudolf <roman_rudolf@icloud.com>
2025-03-27 14:05:09 +0000

    connect button to purchasing product

--- client/pages/products/[productId].js
+++ client/pages/products/[productId].js
@@ -1,13 +1,25 @@
 // Wildcard route: this will be rendered for any URL that matches /products/:productId
 // The productId will be available in the context object
 
+import useRequest from '../../hooks/use-request';
+
 const ProductShow = ({ product }) => {
+  const { doRequest, errors } = useRequest({
+    url: '/api/orders',
+    method: 'post',
+    body: {
+      productId: product.id
+    },
+    onSuccess: (order) => console.log(order)
+  });
+
   return (
     <div>
       <h1>Product Details</h1>
       <h3>Title: {product.title}</h3>
       <h3>Price: £{product.price}</h3>
-      <button className="btn btn-primary">I want to purchase</button>
+      {errors}
+      <button onClick={doRequest} className="btn btn-primary">I want to purchase</button>
     </div>
   );
 }

b523b278223c14d4201776c8b64e7718fb0ef71b
Roman Rudolf <roman_rudolf@icloud.com>
2025-05-07 22:23:57 +0100

    nats-wrapper so it does not connect

--- orders/src/__mocks__/nats-wrapper.ts
+++ orders/src/__mocks__/nats-wrapper.ts
@@ -1,3 +1,5 @@
+// This file mocks the NATS wrapper so that client does not need to connect 
+// to a real NATS server.
 export const natsWrapper = {
   client: {
     publish: jest.fn().mockImplementation(

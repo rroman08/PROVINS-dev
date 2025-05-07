29a30bd7159c2c3c9178334bfe6754e028c79a0f
Roman Rudolf <roman_rudolf@icloud.com>
2025-05-07 14:05:13 +0100

    trying to re-deploy auth service

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get('/api/users/currentuser', currentUser, (req, res) => {
   // It uses the currentUser middleware to check if the user is authenticated
   // and then sends information of the current user back in the response
-  // If the user is not authenticated, it sends null as the currentUser in the response
+  // If the user is not authenticated, it responds with null as instead of the currentUser
   res.send({ currentUser: req.currentUser || null });
 });
 

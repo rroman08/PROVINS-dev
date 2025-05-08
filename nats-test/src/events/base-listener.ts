2fd95a3d9e418c9bbc60f6d90d0bc818371c241e
Roman Rudolf <roman_rudolf@icloud.com>
2025-05-08 14:21:06 +0100

    Updated comments

--- nats-test/src/events/base-listener.ts
+++ nats-test/src/events/base-listener.ts
@@ -48,7 +48,7 @@ export abstract class Listener<T extends Event> {
     });
   }
 
-  // Parse the message data to JSON
+  // Parse the message data
   parseMessage(msg: Message) {
     const data = msg.getData();
     return typeof data === 'string' ? JSON.parse(data) : JSON.parse(data.toString('utf-8'));
